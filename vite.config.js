b95136ce32c4ab480884a76f3ebdea6515f2bb84
KhemMeok <naka4567zin@gmail.com>
2025-09-28 12:55:29 +0700

    Correct base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/', // Use '/' for a user/organization page like 'username.github.io'
   plugins: [
     vue(),
     vueDevTools(),
